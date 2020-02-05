838a0cef613eeec144d7246db4d36e718667c785
Phil Hunt <phil@deviantlogic.co.uk>
2020-02-05 18:00:18 +0000

    big O stff

--- ch1.scm
+++ ch1.scm
@@ -105,3 +105,24 @@
   (cond ((< row col) 0)
         ((or (= row col) (= col 1)) 1)
         (#t (+ (pascal (- row 1) (- col 1)) (pascal (- row 1) col)))))
+
+(define (count-change amount)
+  (cc amount 5))
+
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0)
+             (= kinds-of-coins 0))
+         0)
+        (else
+         (+ (cc amount (- kinds-of-coins 1))
+            (cc (- amount (first-denomination
+                           kinds-of-coins))
+                kinds-of-coins)))))
+
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
